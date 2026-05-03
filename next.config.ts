32ccbd3fba4e4cac8308de39539648f8b9601d8f
davron <saidovdavron61@gmail.com>
2026-05-03 20:33:13 +0500

    f

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Mana shu qatorni qo'shing
+  images: {
+    unoptimized: true, // Statik sayt uchun bu ham kerak bo'ladi
+  },
 };
 
 export default nextConfig;
